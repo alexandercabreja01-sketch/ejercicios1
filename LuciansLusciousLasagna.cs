7fee438670dc862d34514bfc4093c92ea9756430
alexandercabreja01-sketch <alexandercabreja01@gmail.com>
2026-09-10 21:17:18 -0400

    Lasagnaclass

--- LuciansLusciousLasagna.cs
+++ LuciansLusciousLasagna.cs
@@ -1,10 +1,17 @@
+using System;
+
+var lasagna = new Lasagna();
+
+Console.WriteLine("Minutos esperados en el horno: " + lasagna.ExpectedMinutesInOven());
+Console.WriteLine("Minutos restantes (llevaba 30 min): " + lasagna.RemainingMinutesInOven(30));
+Console.WriteLine("Tiempo de preparación (3 capas): " + lasagna.PreparationTimeInMinutes(3));
+Console.WriteLine("Tiempo total transcurrido: " + lasagna.ElapsedTimeInMinutes(3, 30));
 public class Lasagna
 {
     public int ExpectedMinutesInOven()
     {
         return 40;
     }
-
 
     public int RemainingMinutesInOven(int actualMinutes)
     {
@@ -15,6 +22,7 @@ public class Lasagna
     {
         return numberOfLayers * 2;
     }
+
     public int ElapsedTimeInMinutes(int numberOfLayers, int actualMinutesInOven)
     {
         return PreparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
