3eb3ad5e601576bc27b19eae443e0b765f383ae1
alexandercabreja01-sketch <alexandercabreja01@gmail.com>
2026-09-15 20:36:35 -0400

    HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -1,4 +1,14 @@
+using System;
+
 public static class HelloWorld
 {
     public static string Hello() => "Hello, World!";
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(HelloWorld.Hello());
+    }
 }
